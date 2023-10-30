4596c1bb499b6e5b2e40738d614a6e871624bebd
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-10-29 22:42:47 -0300

    Preenchendo os dados do perfil

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,7 @@
 
 async function fetchProfileData() {
-    // const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
 
-    const url = 'https://raw.githubusercontent.com/LuizAraujo2020/web-profile/main/data/profile.json?token=GHSAT0AAAAAACJHBYQKEYJOTUHULLGTKZ3AZJ645WQ'
     const fetching = await fetch(url)
     return await fetching.json()
 }
